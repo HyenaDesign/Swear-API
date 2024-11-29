966089e6e523d9fa79f0ad345085f910a7266557
HyenaDesign <r0952543@student.thomasmore.be>
2024-11-29 14:13:11 +0100

    fix: added CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 const orderRoutes = require('./src/routes/orderRoutes');
 const userRoutes = require('./src/routes/userRoutes');
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/api/v1/orders', orderRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
