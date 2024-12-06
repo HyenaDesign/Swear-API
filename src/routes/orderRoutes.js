ecda89c2f07e7167ba8eeca9a5ed07bcd1aa04f5
FUZZZ <147380081+YungFuzzzz@users.noreply.github.com>
2024-12-06 13:10:49 +0100

    fix: changed orderroutes to be compatible with front-end

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order');
 
-// Haal alle orders op
 router.get('/', async (req, res) => {
   try {
     const orders = await Order.find();
@@ -12,14 +11,25 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Update een order en broadcast de wijziging
+router.get('/:id', async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      return res.status(404).json({ message: 'Order niet gevonden' });
+    }
+    res.status(200).json({ data: order });
+  } catch (error) {
+    res.status(500).json({ message: 'Fout bij ophalen de order', error });
+  }
+});
+
 router.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const updatedOrder = await Order.findByIdAndUpdate(id, req.body, { new: true });
 
     if (updatedOrder) {
-      req.app.get('io').emit('orderUpdated', updatedOrder); // Broadcast update
+      req.app.get('io').emit('orderUpdated', updatedOrder); // Broadcast update naar alle clients
       res.status(200).json(updatedOrder);
     } else {
       res.status(404).json({ message: 'Order niet gevonden' });
