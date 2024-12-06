7eaa47d35be113f886c55cfd44e17cfe048966db
HyenaDesign <r0952543@student.thomasmore.be>
2024-12-06 14:40:07 +0100

    feat: changed orderScheme

--- src/models/Order.js
+++ src/models/Order.js
@@ -8,28 +8,28 @@ const orderSchema = new mongoose.Schema({
     components: {
         laces: {
             color: { type: String, required: true },
-            material: { type: String, required: true }
+            material: { type: String, required: true } // Storing the material as a string identifier
         },
         sole: {
             color: { type: String, required: true },
-            material: { type: String, required: true }
+            material: { type: String, required: true } // Storing the material as a string identifier
         },
         inside: {
             color: { type: String, required: true },
-            material: { type: String, required: true }
+            material: { type: String, required: true } // Storing the material as a string identifier
         },
         outside: {
             part1: {
                 color: { type: String, required: true },
-                material: { type: String, required: true }
+                material: { type: String, required: true } // Storing the material as a string identifier
             },
             part2: {
                 color: { type: String, required: true },
-                material: { type: String, required: true }
+                material: { type: String, required: true } // Storing the material as a string identifier
             },
             part3: {
                 color: { type: String, required: true },
-                material: { type: String, required: true }
+                material: { type: String, required: true } // Storing the material as a string identifier
             }
         }
     }
